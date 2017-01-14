9fda14f48bcdd8aae1be3e45ac390e79a165a761
Andrew Barrett-Sprot <Adimote@users.noreply.github.com>
2017-01-14 13:05:07 +0000

    Neaten main.py
    - Change the #! to /usr/bin/env as it is more flexible 
    - split all imports to a separate line as per PEP 8
    - remove unused imports
    - change file reading to use the `with`
    - use 4 space indentation as per PEP 8

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import sys, time, os, traceback, inspect
-file_path =os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))),"test.txt")
-f = open(file_path,"w")
-f.write("TESTING, I HAVE NO IDEA IF THIS WILL WORK!")
-f.close()
+import os
+import inspect
+
+file_path = os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))),"test.txt")
+
+with open(file_path,"w") as f:
+    f.write("TESTING, I HAVE NO IDEA IF THIS WILL WORK!")
