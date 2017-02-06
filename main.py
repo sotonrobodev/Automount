a77f3a5180e4911fd8ddf455d7bd4b001f28c178
Steffan P <stp1g15@soton.ac.uk>
2017-02-06 21:13:34 +0000

    - main.py has been modified to fit the following changes  - the mounting script now executes the python from the stick's mounted directory  - the udev rule now only works with sda1

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-import inspect
-
-file_path = os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))),"test.txt")
+file_path = "output.log"
 
 with open(file_path,"w") as f:
     f.write("TESTING, I HAVE NO IDEA IF THIS WILL WORK!")
--- mounting_script.sh
+++ mounting_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 pmount /dev/$1
-python3 /media/$1/main.py
+cd /media/$1
+if [ -f main.py ]; then
+  python3 main.py
+else
+  echo "cannot find main.py on this usb stick" > output.log
+fi
+cd /
 pumount /dev/$1
